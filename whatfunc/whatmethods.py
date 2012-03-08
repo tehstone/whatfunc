7a8db4f43710139bd3d70384cb38257ad54882ec
Hiroki Noda <kubo39@gmail.com>
2012-03-09 04:30:37 +0900

    decided method-finder for using 'getattr'

---
+++ whatfunc/whatmethods.py
@@ -0,0 +1,41 @@
+import copy
+
+'''
+function 'what' required applying object, expected result at least.
+'''
+
+def what(obj, expectedResult):
+    MethodFinder.show(obj, expectedResult)
+
+class MethodFinder(object):
+    blacklist = ['daemonize' 'display' 'exec' 'exit' 'fork' 'sleep' 'system'
+                 'syscall' 'what' 'ed' 'emacs' 'mate' 'nano' 'vi' 'vim']
+
+    'required returning seaquenecial object'
+    @classmethod
+    def find(self, anObject, expectedResult):
+        res = filter(lambda x: x not in self.blacklist, dir(type(anObject)))
+
+        def calling(name):
+            try:
+                if getattr(copy.deepcopy(anObject), name)() is expectedResult:
+                    return name
+            except:
+                return False
+            return False
+
+        res = filter(lambda x: calling(x), res)
+        return res
+
+    @classmethod
+    def show(self, anObject, expectedResult):
+        for name in self.find(anObject, expectedResult):
+            print '''%s.%s == %s''' % (
+                repr(anObject),
+                name,
+                repr(expectedResult))
+        return
+
+if __name__ == '__main__':
+    ''' example '''
+    what(3, 3)
